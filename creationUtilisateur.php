23ac7602b638bfd143bf84e0a228b36e34701956
Hidan <rowe.delsinfetch@gmail.com>
2021-10-05 12:03:01 +0200

    ajout control form

--- creationUtilisateur.php
+++ creationUtilisateur.php
@@ -20,25 +20,25 @@
                         <form id="formulaire" name="formulaire" enctype="multipart/form-data" action="" method="post" autocomplete="off">
 
                             <div class="col-md-12">
-                                <input id="nom" class="form-control" name="nom" type="text" placeholder="Nom">
+                                <input id="nom" class="form-control" name="nom" type="text" placeholder="nom" pattern="[a-zA-Z]{3,}" required>
                             </div>
 
                             <div class="col-md-12">
-                                <input id="prenom" class="form-control" name="prenom" type="text" placeholder="prenom">
+                                <input id="prenom" class="form-control" name="prenom" type="text" placeholder="prenom" pattern="[a-zA-Z]{3,}" required>
                             </div>
 
                             <div class="col-md-12">
-                                <input id="email" class="form-control" name="email" type="email" placeholder="E-mail">
+                                <input id="email" class="form-control" name="email" type="email" placeholder="E-mail" title="example@domain.fr" required>
                             </div>                                
 
                             <div class="col-md-12">
-                                <input id="password" class="form-control" name="password" type="password"  placeholder="Saisir votre mot de passe">
+                                <input id="password" class="form-control" name="password" type="password"  placeholder="Saisir votre mot de passe" required>
                             </div>
 
                             <div class="col-md-12 mt-3">
                                 <label class="mb-3 mr-1" for="sexe">Sexe : </label>
 
-                                <input id="homme" class="btn-check" type="radio" name="sexe" value="homme">
+                                <input id="homme" class="btn-check" type="radio" name="sexe" value="homme" checked>
                                 <label class="btn btn-sm btn-outline-secondary" for="homme">Homme</label>
 
                                 <input id="femme" class="btn-check"type="radio"  name="sexe" value="femme">
@@ -48,6 +48,7 @@
                             <div class="col-md-12">
                                 <label for="clubFavori"> Choissisez une équipe favorite :</label>
                                 <select id="clubFavori" list="clubFavoris" name="clubFavori">
+                                    <option value="0">Selection</option>
                                     <?php
                                     foreach ($clubs as $club) {
                                         ?>
@@ -86,7 +87,7 @@
                                 input.addEventListener('change', updateImageDisplay);
                             </script>
                             <div class="form-button mt-3">
-                                <button id="submit" type="submit" name="submit" class="btn btn-primary">S'inscrire</button>
+                                <button id="submit" type="submit" name="validSubmit" class="btn btn-primary">S'inscrire</button>
                             </div>
                         </form>
                     </div>
@@ -94,13 +95,40 @@
             </div>
         </div>
     </div>
+    <script>
+        var boutonSubmit = document.getElementById("submit");
+
+        boutonSubmit.onclick = function (event) {
+
+            var monForm = document.getElementById("formulaire");
+//    var monForm = document.forms("formulaire");
+            var nom = monForm["nom"];
+            var prenom = monForm["prenom"];
+            var mail = monForm["email"];
+            var sexe = monForm["sexe"];
+            var mdp = monForm["password"];
+
+            var emailPregMatch = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/.test(email.value);
+            var nomPregMatch = /[a-zA-Z]{3,}/.test(nom.value);
+            var prenomPregMatch = /[a-zA-Z]{3,}/.test(prenom.value);
+            var mdpPregMatch = mdp != "";
+
+            if (emailPregMatch && nomPregMatch && prenomPregMatch && mdpPregMatch) {
+                document.write("ok");
+            }else{
+                event.preventDefault();
+            }
+        }
+
+
+
+    </script>
     <div style="color:#fff;">
         <?php
 //        // Vérifie que le boutton à été cliquer
         if (isset($_POST['submit'])) {
 //            // Affichade de variable
             dump($_POST);
-//            updateUtilisateur('1', $_POST);
             // enregistre l'utilisateur
 //            $id = signUser($_POST);
 //
@@ -127,10 +155,10 @@
 //            if (isset($_POST['clubNews'])) {
 //                subscribeClub($id, $_POST['clubNews']);
 //        }
-        ?>
-    </div>
-    <?php
-        }
+            ?>
+        </div>
+        <?php
+    }
     ?>
 </body>
 </html>
